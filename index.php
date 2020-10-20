381abbb6d423defc0552c9f679eb8fbfa1471ebb
Donald-Mahanahan <ZachSherwood@boisestate.edu>
2020-10-19 20:19:48 -0600

    wip

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
  <?php
- session_start();
- require_once dirname(__FILE__). '/Dao.php';
-     
- $dao = new Dao();
+    session_start();
+    require_once dirname(__FILE__). '/dao.php';
+    error_reporting(E_ALL);
+        ini_set('display_errors', 1);
+    $dao = new Dao();
  ?>
  
  
