e86bc6116eb53bf678048a3c2f3a0d93d3ff85ce
Donald-Mahanahan <ZachSherwood@boisestate.edu>
2020-10-19 20:24:10 -0600

    wip

--- index.php
+++ index.php
@@ -57,8 +57,8 @@
                  
                  <?php
                   $users = $dao->test();
-                  echo $users;
-                  echo $users[0];
+                  print_r($users);
+                //   echo $users[0];
                  ?>
                  
              </form>
