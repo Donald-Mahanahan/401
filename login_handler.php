32db45754fb6f5d82fa5e1f79ff4988470f883b5
Donald-Mahanahan <ZachSherwood@boisestate.edu>
2020-10-29 22:11:31 -0600

    wip

--- login_handler.php
+++ login_handler.php
@@ -2,7 +2,7 @@
  error_reporting(E_ALL);
   ini_set('display_errors', 1);
 
-require_once dirname(__FILE__). '/../Dao.php';
+require_once dirname(__FILE__). '/../dao.php';
 //  require_once '/Dao.php';
 
  session_start();
@@ -11,6 +11,37 @@ require_once dirname(__FILE__). '/../Dao.php';
  
  $regex = "/\w{1,20}/";
 
+ if(!preg_match($regex,$_POST['userName']) || !preg_match($regex,$_POST['password'])){
+  $_SESSION['auth'] = false;
+  $_SESSION['message'] = "Invalid username or password. They must be 1 - 20 characters long and contain alpha-numeric characters only";
+  $_SESSION['form'] = $_POST;
+  header("Location: https://polar-sands-59708.herokuapp.com/login.php");
+  exit;
+}
+
+echo "test error1";
+
+$results = $dao->userExists($_POST['userName'], $_POST['password']);
+print_r($results);
+
+
+// $dao = new Dao();
+// $dao->getUsername($_POST['comment']);
+// $_SESSION['good'][] = "Thank you for posting";
+
+unset($_SESSION['form']);
+if($results){
+    $_SESSION['auth'] = true;
+    $_SESSION['user_id'] = $results['user_id'];
+    header("Location: https://polar-sands-59708.herokuapp.com/");
+    exit;
+} else{
+    $_SESSION['auth'] = false;
+    $_SESSION['message'] = "Invalid username or password";
+    $_SESSION['form'] = $_POST;
+    header("Location: https://polar-sands-59708.herokuapp.com/login.php");
+    exit;
+}
 
 
 // if (($_POST['username'] == 'zach') && ($_POST['password'] == '5678')) {
