f2d2f6d968a3b380e85e4aef1ce21bbfdf8dcefa
Donald-Mahanahan <ZachSherwood@boisestate.edu>
2020-10-29 22:05:59 -0600

    wip

--- login_handler.php
+++ login_handler.php
@@ -1,8 +1,8 @@
 <?php
  error_reporting(E_ALL);
   ini_set('display_errors', 1);
-  
-require_once dirname(__FILE__). '/Dao.php';
+
+require_once dirname(__FILE__). '/../Dao.php';
 //  require_once '/Dao.php';
 
  session_start();
