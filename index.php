dcac2006ac293bf0435edbe69840dab2c8be5ddd
Donald-Mahanahan <ZachSherwood@boisestate.edu>
2020-11-23 20:23:52 -0700

    wer

--- index.php
+++ index.php
@@ -88,26 +88,11 @@
              id: 'kNybja6',
              name: 'Mom\'s Birthday',
              description: 'Lorem ipsum dolor sit..',
-             date: 'May 27, 2020',
+             date: 'November 23, 2020',
              type: 'birthday'
         });
-        // add multiple events
-        $('#calendar').evoCalendar('addCalendarEvent', [
-             {
-                  id: 'kNybja6',
-                  name: 'Mom\'s Birthday',
-                  date: 'May 27, 1965',
-                  type: 'birthday',
-                  everyYear: true // optional
-             },
-             {
-                  id: 'asDf87L',
-                  name: 'Graduation Day!',
-                  date: 'March 21, 2020',
-                  type: 'event'
-             }
-]);
-         })
+
+    })
 
          
 
