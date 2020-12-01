3474928596705b2ef954a565eeb4647e69f86e25
Donald-Mahanahan <ZachSherwood@boisestate.edu>
2020-12-01 15:36:24 -0700

    wip

--- index.php
+++ index.php
@@ -30,9 +30,7 @@
 
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-    <script src="js/myScript.js"></script>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="myScript.js"></script>
 
 
  </head>
