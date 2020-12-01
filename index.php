ba0d084ff3d42912b5a7dda4b119e767e35305c3
Donald-Mahanahan <ZachSherwood@boisestate.edu>
2020-12-01 15:38:21 -0700

    qwe

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-    <script src="myScript.js"></script>
+    <script src="js/myScript.js"></script>
 
 
  </head>
