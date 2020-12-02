c767c27779c2c3c80dbce1d9a9628f6b59d6d090
Donald-Mahanahan <ZachSherwood@boisestate.edu>
2020-12-02 16:27:50 -0700

    Revert "Create README.md"
    This reverts commit 8f80e31097313bcb00f7dae047904df703947734.

--- delete_handler.php
+++ delete_handler.php
@@ -9,32 +9,15 @@ require_once "$rootDir/dao.php";
 
 
 session_start();
-// require_once 'KLogger.php';
-// $logger = new KLogger ("log.txt" , KLogger::DEBUG);
 $_SESSION['bad'] = array();
 $_SESSION['good'] = array();
 
-// validating
-
-
-// if (strlen($_POST['task']) == 0) {
-//   $_SESSION['bad'][] = "Please enter a comment";
-// }
-
-// if (strlen($_POST['task']) > 256) {
-//   $_SESSION['bad'][] = "Comment is too long.";
-// }
-
-// if (count($_SESSION['bad']) > 0) {
-//   header("Location: https://polar-sands-59708.herokuapp.com/");
-//   exit();
-// }
 
 $dao = new Dao();
 
 $dao->deleteTask($_POST['task']);
 $_SESSION['good'][] = "Thank you for posting";
 
-// redirect back to the comments page
+
 header("Location: https://polar-sands-59708.herokuapp.com/");
 exit();
