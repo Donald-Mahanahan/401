d609334236abda69b1eb7837872210f68ca71740
Donald-Mahanahan <ZachSherwood@boisestate.edu>
2020-12-02 15:34:13 -0700

    qwe

--- index.php
+++ index.php
@@ -58,10 +58,10 @@
                         <?php
                             if (isset($_SESSION['good'])) {
                                 foreach ($_SESSION['good'] as $message) {
-                                echo "<div class='fadeout'>{$message}</div>";
+                                echo "<div class='fadeout'><span class='fadeout'>{$message}</span></div>";
                                 }
                                 foreach ($_SESSION['bad'] as $message) {
-                                echo "<div class='fadeout'>{$message}</div>";
+                                echo "<div class='fadeout'><span class='fadeout'>{$message}</span></div>";
                                 }
                             }
 
